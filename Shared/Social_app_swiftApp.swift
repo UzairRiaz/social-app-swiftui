10555b5f2f8d939f3e0d5cdb10fa2c7ff097e7fa
uzairriaz <uzairriaz.pk@gmail.com>
2021-08-14 18:59:57 +0500

    Initial Commit

---
+++ Shared/Social_app_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_app_swiftApp.swift
+//  Shared
+//
+//  Created by Umair Riaz on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Social_app_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
